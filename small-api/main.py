18b5a8b0335d8f692d46a73d86a7f8222d4e5946
David Vicente <davidvicenteblanco@gmail.com>
2023-02-20 18:08:03 +0100

    done

---
+++ small-api/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import random
+from tools.sql_queries import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello world!"
+
+@app.route("/random-number")
+def random_number ():
+    return str(random.randint(0, 10))
+
+@app.route('/everything-employees')
+def example():
+    return jsonify(get_everything())
+
+@app.route('/table/<one_table>')
+def any_table(one_table):
+    return jsonify(table_ten(one_table))
+
+if __name__ == "__main__":
+     app.run(port=7070, debug=False)
+
